7af4d12a2a75c8f57f61f7e31dff4bd519741e39
meenabisht <meena.bisht392@gmail.com>
2019-01-23 11:04:45 +0530

    modified welcome.php

--- welcome.php
+++ welcome.php
@@ -3,10 +3,10 @@ echo"you are logged in";
 ?>
 
 <html>
-    <head></head>
-    <body>
-        <div>
-            <button><a href="logout.php">Logout</a></button>
-        </div>
-    </body>
+  <head></head>
+  <body>
+    <div>
+      <button><a href="logout.php">Logout</a></button>
+    </div>
+  </body>
 </html>
