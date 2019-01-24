4a588f9ffc08420c554492d2713013ac7c102449
meenabisht <meena.bisht392@gmail.com>
2019-01-24 08:45:55 +0530

    name changed of the file

--- Register.php
+++ Register.php
@@ -1,21 +1,16 @@
- <html>
-<body>
-<?php  
-
+<?php
+include("Register.php");
 include("user.php");
-
-$user_obj = new user();
-
-  $name=$_POST["name"];
-  $addr=$_POST["addr"];
-  $email=$_POST["email"];
-  $mobno=$_POST["mobno"];
-  $high_qual=$_POST["high_qual"];  
-  $user_obj->UserRegister($name, $addr, $email, $mobno, $high_qual);
-
-  $db_object=new DbConnect(); 
-  print_r($db_object);
+if($_SERVER["REQUEST_METHOD"]=="POST")
+{  
+  $reg_obj=new user();
+  $result1=$reg_obj->userregister($_POST['name'],$_POST['addr'],$_POST['email'],$_POST['mobno'],$_POST['high_qual']);
+  if(!$result1)
+  {
+  echo"not Inserted";}
+  else
+  {
+      echo"Inserted";
+  }
+}
 ?>
-
-</body>
-</html>
